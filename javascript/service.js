f679738a44ef76d87b89c1c880998c3c848df1b1
2gyuho <164995024+2gyuho@users.noreply.github.com>
2024-06-04 23:15:00 +0900

    service.js 기능추가

---
+++ javascript/service.js
@@ -0,0 +1,37 @@
+function updateActiveLink(newActiveLink) {
+    document.querySelectorAll('.service-link').forEach(link => {
+        link.classList.remove('active');
+    });
+    newActiveLink.classList.add('active');
+}
+
+function unknownfunction() {
+    document.querySelectorAll('.service-link').forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            document.querySelectorAll('.content-section').forEach(section => {
+                section.style.display = 'none';
+            });
+            document.getElementById(this.dataset.content).style.display = 'block';
+            updateActiveLink(this);
+        });
+    });
+}
+
+function changeTheme() {
+    const header = document.querySelector('.header');
+    const footer = document.querySelector('.footer');
+    const themeColor = localStorage.getItem("theme");
+    header.style.backgroundColor = themeColor;
+    footer.style.backgroundColor = themeColor;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    unknownfunction();
+    changeTheme();
+    const firstLink = document.querySelector('.service-link');
+    if (firstLink) {
+        firstLink.classList.add('active');
+        document.getElementById(firstLink.dataset.content).style.display = 'block';
+    }
+});
